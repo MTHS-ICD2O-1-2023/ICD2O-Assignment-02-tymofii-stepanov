95da8e7c0da919f33ba140a9ff43f9e87c497182
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-03-26 18:17:56 +0000

    End with assigment 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates Perimeter of a triangle.
+ */
+function calculatePerimeterOfTriangle() {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const firstSide = parseInt(document.getElementById('first-side').value)
+  const secondSide = parseInt(document.getElementById('second-side').value)
+
+
+  // process
+  const perimeterOfTriangle = baseOfTriangle + firstSide + secondSide
+
+  // output
+  document.getElementById('perimeter').innerHTML = 'Area is: ' + perimeterOfTriangle + ' cm'
+}
